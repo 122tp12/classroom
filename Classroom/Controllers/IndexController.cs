c323b3a1cb12fa1b3b9402b160d6e1283098ef1b
122tp12 <63251202+122tp12@users.noreply.github.com>
2021-10-07 19:42:29 +0300

    Добавьте файлы проекта.

---
+++ Classroom/Controllers/IndexController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Classroom.Models.Index;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Classroom.Controllers
+{
+ 
+
+    public class IndexController : Controller
+    {
+        public IHttpContextAccessor accessor;
+        //Just taking accessor for session
+        public IndexController(IHttpContextAccessor _accessor)
+        {
+            accessor = _accessor;
+
+        }
+       
+        public IActionResult Index()
+        {
+            IndexModel model=new IndexModel(accessor);
+
+            return View(model);
+        }
+    }
+}
---
+++ Classroom/Models/Index/IndexModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classroom.Models.Index
+{
+    public class IndexModel : PageModel
+    {
+        IHttpContextAccessor accessor;
+        public IndexModel(IHttpContextAccessor _accessor)
+        {
+            accessor = _accessor;
+        }
+    }
+}
